a601e4d546ecba6ca52d7b1cd345db14c33bb8a8
DeepBlue <1012946585@qq.com>
2019-02-16 22:30:04 +0800

    第二次提交

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -2,7 +2,7 @@ package com.coolweather.android.db;
 
 import org.litepal.crud.DataSupport;
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
     private String countyName;
     private String weatherId;
